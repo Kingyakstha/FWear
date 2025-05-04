a5772a98850ef7a7d31b6d4a0b4cefb78fd0d2f9
Kingyakstha <kingyakshrestha123@gmail.com>
2025-05-04 13:41:50 +0545

    fixing database

--- Backend-Ecom/src/db/index.js
+++ Backend-Ecom/src/db/index.js
@@ -3,12 +3,8 @@ import { DB_NAME } from '../constants.js';
 
 const connectDB = async ()=>{
     try {
-        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            ssl: true,
-            tlsAllowInvalidCertificates: false
-        })
+        
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n Mongodb connected !! DB host : ${connectionInstance.connection.host}`)
         
     } catch (error) {
