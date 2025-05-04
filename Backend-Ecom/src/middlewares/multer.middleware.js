f2efe1ea56a6b6f9f1842a640a72cc484d6df05f
Kingyakstha <kingyakshrestha123@gmail.com>
2025-05-04 14:05:01 +0545

    almost fixed the database upload function

--- Backend-Ecom/src/middlewares/multer.middleware.js
+++ Backend-Ecom/src/middlewares/multer.middleware.js
@@ -1,8 +1,13 @@
 import multer from "multer";
+import fs from "fs";
+import path from "path";
+import os from "os";
 
+const localPath = path.join("src", "public", "temp",'hello');
+console.log("Local path is ",localPath," and ",os.tmpdir())
 const storage = multer.diskStorage({    // stores the image in device then uploads
     destination: function (req, file, cb) {
-      cb(null, "src/public/temp")
+      cb(null,fs.existsSync(localPath)? "src/public/temp":os.tmpdir())
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname)
